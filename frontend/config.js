1d0936c3c83633a803f5613ceec23ae6ba4f44e5
hyperclick <1@hyperclick.com>
2026-08-19 18:36:58 +0300

    3

--- frontend/config.js
+++ frontend/config.js
@@ -7,5 +7,5 @@ const CONFIG = {
     // ИИ или вы сами измените этот адрес перед публикацией
     API_ROOT: isLocal 
         ? 'http://localhost:3000/api/' 
-        : 'https://my-api.layero.app/api/'
+        : '/api/'
 };
